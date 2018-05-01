0d581b90c9f52b5e4bdd5b2585a8e47e9b2c57fc
gromax <gomaxx@gmail.com>
2018-05-01 12:03:42 +0200

    Version 2.2.0 mise en production

--- app/app.js
+++ app/app.js
@@ -44,7 +44,7 @@ define(["marionette","jquery-ui","bootstrap"], function(Marionette){
 	});
 
 	Manager.on("start", function(){
-		Manager.version = "2.1.25";
+		Manager.version = "2.2.0";
 		var historyStart = function() {
 			require([
 				"apps/header/header_app",
