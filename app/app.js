852d8273fe96ef286de936bfad81ff2cd0b5057b
gromax <gomaxx@gmail.com>
2018-05-05 21:14:50 +0200

    version 2.2.79

--- app/app.js
+++ app/app.js
@@ -52,7 +52,7 @@ define(["marionette","jquery-ui","bootstrap"], function(Marionette){
 
 
 	Manager.on("start", function(){
-		Manager.version = "2.2.65";
+		Manager.version = "2.2.79";
 		var historyStart = function() {
 			require([
 				"apps/header/header_app",
