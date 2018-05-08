ec661e5d872db5e2bfd4d588878e0e3769c7e023
gromax <gomaxx@gmail.com>
2018-05-08 23:50:41 +0200

    Fichier passé en version coffee Lors de la création d'un devoir, il faut forcer le nom du propriétaire qui n'est pas renvoyé par l'api (inutile, c'est forcément le connecté)

--- app/apps/devoirs/list/list_controller.js
+++ app/apps/devoirs/list/list_controller.js
@@ -1,144 +1,124 @@
-define([
-	"app",
-	"marionette",
-	"apps/common/alert_view",
-	"apps/common/list_layout",
-	"apps/devoirs/list/list_panel",
-	"apps/devoirs/list/list_view",
-	"apps/devoirs/edit/edit_fiche_view"
-], function(
-	app,
-	Marionette,
-	AlertView,
-	Layout,
-	Panel,
-	ListView,
-	ShowView
-){
-
-	var Controller = Marionette.Object.extend({
-		channelName: 'entities',
-
-		list: function(){
-			app.trigger("header:loading", true);
-			var listItemsLayout = new Layout();
-			var listItemsPanel = new Panel();
-			var channel = this.getChannel();
-
-			require(["entities/devoir", "entities/dataManager"], function(Item){
-				var fetching = channel.request("custom:entities",["fiches"]);
-				$.when(fetching).done(function(fiches){
-					var listItemsView = new ListView({
-						collection: fiches
-					});
-
-					listItemsLayout.on("render", function(){
-						listItemsLayout.getRegion('panelRegion').show(listItemsPanel);
-						listItemsLayout.getRegion('itemsRegion').show(listItemsView);
-					});
-
-					listItemsPanel.on("devoir:new", function(){
-						var newItem = new Item();
-						var view = new ShowView({
-							model: newItem,
-							editMode: true,
-						});
-
-						view.on("form:submit", function(data){
-							var savingItem = newItem.save(data);
-							if (savingItem){
-								app.trigger("header:loading", true);
-								$.when(savingItem).done(function(){
-									fiches.add(newItem);
-									view.trigger("dialog:close");
-									listItemsView.flash(newItem);
-								}).fail(function(response){
-									if(response.status == 422){
-										view.triggerMethod("form:data:invalid", response.responseJSON.errors);
-									} else {
-										if(response.status == 401){
-											alert("Vous devez vous (re)connecter !");
-											view.trigger("dialog:close");
-											app.trigger("home:logout");
-										} else {
-											alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code "+response.status+"/020]");
-										}
-									}
-								}).always(function(){
-									app.trigger("header:loading", false);
-								});
-							} else {
-								view.triggerMethod("form:data:invalid",newItem.validationError);
-							}
-
-						});
-						app.regions.getRegion('dialog').show(view);
-					});
-
-					listItemsView.on("item:show",function(childView, args){
-						var model = childView.model;
-						app.trigger("devoir:show", model.get("id"));
-					});
-
-					listItemsView.on("item:setAttribute", function(childView, attr_name){
-						// attr_name sera soit visible soit actif
-						var model = childView.model;
-						var attr_value = model.get(attr_name);
-						model.set(attr_name, !attr_value);
-						var updatingItem = model.save();
-						if (updatingItem) {
-							app.trigger("header:loading", true);
-							$.when(updatingItem).done(function(){
-								childView.render();
-								childView.flash("success");
-							}).fail(function(response){
-								if(response.status == 401){
-									alert("Vous devez vous (re)connecter !");
-									app.trigger("home:logout");
-								} else {
-									alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code "+response.status+"/021]");
-								}
-							}).always(function(){
-								app.trigger("header:loading", false);
-							});
-						} else {
-							alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code x/022]");
-						}
-					});
-
-					listItemsView.on("item:delete", function(childView,e){
-						var model = childView.model;
-						var idFiche = model.get("id");
-						if (confirm("Supprimer le devoir « "+model.get("nom")+" » ?")) {
-							var destroyRequest = model.destroy();
-							app.trigger("header:loading", true);
-							$.when(destroyRequest).done(function(){
-								childView.remove();
-								channel.request("fiche:destroy:update", idFiche);
-							}).fail(function(response){
-								alert("Erreur. Essayez à nouveau !");
-							}).always(function(){
-								app.trigger("header:loading", false);
-							});
-						}
-
-					});
-
-					app.regions.getRegion('main').show(listItemsLayout);
-				}).fail(function(response){
-					if(response.status == 401){
-						alert("Vous devez vous (re)connecter !");
-						app.trigger("home:logout");
-					} else {
-						var alertView = new AlertView();
-						app.regions.getRegion('main').show(alertView);
-					}
-				}).always(function(){
-					app.trigger("header:loading", false);
-				})
-			});
-		}
-	});
-
-	return new Controller();
+define(["app", "marionette", "apps/common/alert_view", "apps/common/list_layout", "apps/devoirs/list/list_panel", "apps/devoirs/list/list_view", "apps/devoirs/edit/edit_fiche_view"], function(app, Marionette, AlertView, Layout, Panel, ListView, ShowView) {
+  var Controller;
+  Controller = Marionette.Object.extend({
+    channelName: 'entities',
+    list: function() {
+      var channel, listItemsLayout, listItemsPanel;
+      app.trigger("header:loading", true);
+      listItemsLayout = new Layout();
+      listItemsPanel = new Panel();
+      channel = this.getChannel();
+      return require(["entities/devoir", "entities/dataManager"], function(Item) {
+        var fetching;
+        fetching = channel.request("custom:entities", ["fiches"]);
+        return $.when(fetching).done(function(fiches) {
+          var listItemsView;
+          listItemsView = new ListView({
+            collection: fiches
+          });
+          listItemsLayout.on("render", function() {
+            listItemsLayout.getRegion('panelRegion').show(listItemsPanel);
+            return listItemsLayout.getRegion('itemsRegion').show(listItemsView);
+          });
+          listItemsPanel.on("devoir:new", function() {
+            var newItem, view;
+            newItem = new Item();
+            view = new ShowView({
+              model: newItem,
+              editMode: true
+            });
+            view.on("form:submit", function(data) {
+              var savingItem;
+              savingItem = newItem.save(data);
+              if (savingItem) {
+                app.trigger("header:loading", true);
+                return $.when(savingItem).done(function() {
+                  newItem.set("nomOwner", app.Auth.get("nom"));
+                  fiches.add(newItem);
+                  view.trigger("dialog:close");
+                  return listItemsView.flash(newItem);
+                }).fail(function(response) {
+                  switch (response.status) {
+                    case 422:
+                      return view.triggerMethod("form:data:invalid", response.responseJSON.errors);
+                    case 401:
+                      alert("Vous devez vous (re)connecter !");
+                      view.trigger("dialog:close");
+                      return app.trigger("home:logout");
+                    default:
+                      return alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code " + response.status + "/020]");
+                  }
+                }).always(function() {
+                  return app.trigger("header:loading", false);
+                });
+              } else {
+                return view.triggerMethod("form:data:invalid", newItem.validationError);
+              }
+            });
+            return app.regions.getRegion('dialog').show(view);
+          });
+          listItemsView.on("item:show", function(childView, args) {
+            var model;
+            model = childView.model;
+            return app.trigger("devoir:show", model.get("id"));
+          });
+          listItemsView.on("item:setAttribute", function(childView, attr_name) {
+            var attr_value, model, updatingItem;
+            model = childView.model;
+            attr_value = model.get(attr_name);
+            model.set(attr_name, !attr_value);
+            updatingItem = model.save();
+            if (updatingItem) {
+              app.trigger("header:loading", true);
+              return $.when(updatingItem).done(function() {
+                childView.render();
+                return childView.flash("success");
+              }).fail(function(response) {
+                if (response.status === 401) {
+                  alert("Vous devez vous (re)connecter !");
+                  return app.trigger("home:logout");
+                } else {
+                  return alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code " + response.status + "/021]");
+                }
+              }).always(function() {
+                return app.trigger("header:loading", false);
+              });
+            } else {
+              return alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code x/022]");
+            }
+          });
+          listItemsView.on("item:delete", function(childView, e) {
+            var destroyRequest, idFiche, model;
+            model = childView.model;
+            idFiche = model.get("id");
+            if (confirm("Supprimer le devoir « " + (model.get('nom')) + " » ?")) {
+              destroyRequest = model.destroy();
+              app.trigger("header:loading", true);
+              return $.when(destroyRequest).done(function() {
+                childView.remove();
+                return channel.request("fiche:destroy:update", idFiche);
+              }).fail(function(response) {
+                return alert("Erreur. Essayez à nouveau !");
+              }).always(function() {
+                return app.trigger("header:loading", false);
+              });
+            }
+          });
+          return app.regions.getRegion('main').show(listItemsLayout);
+        }).fail(function(response) {
+          var alertView;
+          if (response.status === 401) {
+            alert("Vous devez vous (re)connecter !");
+            return app.trigger("home:logout");
+          } else {
+            alertView = new AlertView();
+            return app.regions.getRegion('main').show(alertView);
+          }
+        }).always(function() {
+          return app.trigger("header:loading", false);
+        });
+      });
+    }
+  });
+  return new Controller();
 });
---
+++ dev/coffee/apps/devoirs/list/list_controller.coffee
@@ -0,0 +1,126 @@
+define [
+	"app",
+	"marionette",
+	"apps/common/alert_view",
+	"apps/common/list_layout",
+	"apps/devoirs/list/list_panel",
+	"apps/devoirs/list/list_view",
+	"apps/devoirs/edit/edit_fiche_view"
+], (
+	app,
+	Marionette,
+	AlertView,
+	Layout,
+	Panel,
+	ListView,
+	ShowView
+) ->
+
+	Controller = Marionette.Object.extend {
+		channelName: 'entities'
+
+		list: ->
+			app.trigger("header:loading", true)
+			listItemsLayout = new Layout()
+			listItemsPanel = new Panel()
+			channel = @getChannel()
+
+			require ["entities/devoir", "entities/dataManager"], (Item) ->
+				fetching = channel.request("custom:entities",["fiches"])
+				$.when(fetching).done( (fiches)->
+					listItemsView = new ListView { collection: fiches }
+
+					listItemsLayout.on "render", ()->
+						listItemsLayout.getRegion('panelRegion').show(listItemsPanel)
+						listItemsLayout.getRegion('itemsRegion').show(listItemsView)
+
+					listItemsPanel.on "devoir:new", ()->
+						newItem = new Item();
+						view = new ShowView({
+							model: newItem
+							editMode: true
+						})
+
+						view.on "form:submit", (data) ->
+							savingItem = newItem.save(data)
+							if savingItem
+								app.trigger("header:loading", true)
+								$.when(savingItem).done( ()->
+									newItem.set("nomOwner", app.Auth.get("nom")) # L'api ne renvoie pas le nom du créateur qui est forcément le connecté
+									fiches.add(newItem)
+									view.trigger("dialog:close")
+									listItemsView.flash(newItem)
+								).fail( (response)->
+									switch response.status
+										when 422
+											view.triggerMethod("form:data:invalid", response.responseJSON.errors)
+										when 401
+											alert("Vous devez vous (re)connecter !")
+											view.trigger("dialog:close")
+											app.trigger("home:logout")
+										else
+											alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code "+response.status+"/020]")
+								).always( ()->
+									app.trigger("header:loading", false)
+								)
+							else
+								view.triggerMethod("form:data:invalid",newItem.validationError)
+
+						app.regions.getRegion('dialog').show(view)
+
+					listItemsView.on "item:show", (childView, args)->
+						model = childView.model
+						app.trigger("devoir:show", model.get("id"))
+
+					listItemsView.on "item:setAttribute", (childView, attr_name)->
+						# attr_name sera soit visible soit actif
+						model = childView.model
+						attr_value = model.get(attr_name)
+						model.set(attr_name, !attr_value)
+						updatingItem = model.save()
+						if updatingItem
+							app.trigger("header:loading", true)
+							$.when(updatingItem).done( ()->
+								childView.render()
+								childView.flash("success")
+							).fail( (response)->
+								if response.status is 401
+									alert("Vous devez vous (re)connecter !")
+									app.trigger("home:logout")
+								else
+									alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code "+response.status+"/021]")
+							).always( ()->
+								app.trigger("header:loading", false)
+							)
+						else
+							alert("Erreur inconnue. Essayez à nouveau ou prévenez l'administrateur [code x/022]")
+
+					listItemsView.on "item:delete", (childView,e) ->
+						model = childView.model
+						idFiche = model.get("id")
+						if confirm("Supprimer le devoir « #{model.get('nom')} » ?")
+							destroyRequest = model.destroy()
+							app.trigger("header:loading", true)
+							$.when(destroyRequest).done( ()->
+								childView.remove()
+								channel.request("fiche:destroy:update", idFiche)
+							).fail( (response)->
+								alert("Erreur. Essayez à nouveau !")
+							).always( ()->
+								app.trigger("header:loading", false)
+							)
+
+					app.regions.getRegion('main').show(listItemsLayout)
+				).fail( (response)->
+					if response.status is 401
+						alert("Vous devez vous (re)connecter !")
+						app.trigger("home:logout")
+					else
+						alertView = new AlertView()
+						app.regions.getRegion('main').show(alertView)
+				).always( ()->
+					app.trigger("header:loading", false)
+				)
+	}
+
+	return new Controller()
