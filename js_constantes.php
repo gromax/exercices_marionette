6104a14c89ea8c7e2aca0011e1c86ba862b76b5b
gromax <gomaxx@gmail.com>
2018-05-13 23:52:21 +0200

    Fichiers devenus inutiles

--- js_constantes.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include "./php/constantes.php"
-?>
-	var PSEUDO_MIN_SIZE = "<?php echo PSEUDO_MIN_SIZE; ?>";
-	var PSEUDO_MAX_SIZE = "<?php echo PSEUDO_MAX_SIZE; ?>";
-	var NOMCLASSE_MIN_SIZE = "<?php echo NOMCLASSE_MIN_SIZE; ?>";
-	var NOMCLASSE_MAX_SIZE = "<?php echo NOMCLASSE_MAX_SIZE; ?>";
