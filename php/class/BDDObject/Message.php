6ff9bbc91054032bb56874e17bf11a33b9c94e2f
gromax <gomaxx@gmail.com>
2018-07-29 19:40:01 +0200

    Modif : un message ne peut avoir qu'un dest. plus besoin des assoc dest/message

--- php/class/BDDObject/Message.php
+++ php/class/BDDObject/Message.php
@@ -19,7 +19,9 @@ final class Message extends Item
 			'idOwner' => array( 'def' => null, 'type' => 'integer'),	// id de l'auteur
 			'message' => array( 'def' => "", 'type'=> 'string'),		// contenu du message
 			'aUE' => array( 'def' => "", 'type' => 'integer'),	// données relatives au contexte
-			'date' => array( 'def' => date('Y-m-d H:i:s'), 'type' => 'dateHeure')	// Date-heure de création
+			'date' => array( 'def' => date('Y-m-d H:i:s'), 'type' => 'dateHeure'),	// Date-heure de création
+			'lu' => array( 'def' => false, 'type'=>'boolean'),
+			'idDest' => array( 'def' => null, 'type' => 'integer')
 			);
 	}
 
@@ -28,11 +30,9 @@ final class Message extends Item
 		require_once BDD_CONFIG;
 		try {
 			// en tant qu'expéditeur
-			//$expediteur_bdd_result = DB::query("SELECT m.id, m.idOwner, m.message, m.typeContext, m.dataContext, m.date, GROUP_CONCAT(d.idDest SEPARATOR ';') AS dests, 1 AS lu FROM (".PREFIX_BDD."messages m JOIN ".PREFIX_BDD."destMessages d ON d.idMessage = m.id) WHERE m.idOwner=%i", $idUser);
-			$expediteur_bdd_result = DB::query("SELECT m.id, m.idOwner, m.message, m.aUE, m.date, 'Moi' AS ownerName, GROUP_CONCAT(CONCAT(d.idDest,':',u.nom,' ',u.prenom) SEPARATOR ';') AS dests, 1 AS lu FROM ((".PREFIX_BDD."messages m JOIN ".PREFIX_BDD."destMessages d ON d.idMessage = m.id) JOIN ".PREFIX_BDD."users u ON u.id=d.idDest) WHERE m.idOwner=%i GROUP BY m.id", $idUser);
+			$expediteur_bdd_result = DB::query("SELECT m.id, m.idOwner, m.message, m.aUE, m.date, 'Moi' AS ownerName, m.idDest, CONCAT(u.nom,' ',u.prenom) AS destName, 1 AS lu FROM (".PREFIX_BDD."messages m JOIN ".PREFIX_BDD."users u ON u.id=m.idDest) WHERE m.idOwner=%i", $idUser);
 			// en tant que récepteur
-			//$recepteur_bdd_result=DB::query("SELECT m.id, m.idOwner, m.message, m.typeContext, m.dataContext, m.date, ".$idUser." AS dests, d.lu FROM (".PREFIX_BDD."messages m JOIN ".PREFIX_BDD."destMessages d ON d.idMessage = m.id) WHERE d.idDest=%i", $idUser);
-			$recepteur_bdd_result=DB::query("SELECT m.id, m.idOwner, m.message, m.aUE, m.date, CONCAT(u.prenom, ' ', u.nom) AS ownerName, '".$idUser.":Moi' AS dests, d.lu FROM ((".PREFIX_BDD."messages m JOIN ".PREFIX_BDD."destMessages d ON d.idMessage = m.id) JOIN ".PREFIX_BDD."users u ON u.id=m.idOwner) WHERE d.idDest=%i", $idUser);
+			$recepteur_bdd_result=DB::query("SELECT m.id, m.idOwner, m.message, m.aUE, m.date, CONCAT(u.prenom, ' ', u.nom) AS ownerName, '".$idUser."' AS idDest, 'Moi' AS destName, m.lu FROM (".PREFIX_BDD."messages m JOIN ".PREFIX_BDD."users u ON u.id = m.idOwner) WHERE m.idDest=%i", $idUser);
 		} catch(MeekroDBException $e) {
 			EC::addBDDError($e->getMessage(), "Messages/getList");
 			return array("error"=>true, "message"=>$e->getMessage());
@@ -43,34 +43,62 @@ final class Message extends Item
 		return $bdd_result;
 	}
 
-	protected static function getAssocs()
+
+	public static function unReadNumber($idUser)
 	{
-		return array("destMessages" => "idMessage");
+		require_once BDD_CONFIG;
+		try{
+			DB::query("SELECT id FROM ".PREFIX_BDD.static::$BDDName." WHERE idDest = %i AND lu = 0", $idUser);
+			return DB::count();
+		} catch(MeekroDBException $e) {
+			EC::addBDDError($e->getMessage(), 'Messages/unReadNumber');
+		}
+		return 0;
 	}
 
 
 	##################################### METHODES #####################################
 
 
-	protected function okToUpdate($params)
-	{
-		return false;
-	}
-
-	protected function assocsToInsert()
-	{
-		$dests = explode(";", $this->cibles);
-		return array_map($this->mapAssoc, $dests);
-	}
-
-	private function mapAssoc($id)
-	{
-		return array("idDest"=> $id, "idMessage"=>$this->id, "read"=>false);
-	}
-
 	public function isOwnedBy($user)
 	{
 		return $this->values['idOwner'] == $user->getId();
+	}
+
+	public function isDestTo($user)
+	{
+		return $this->values['idDest'] == $user->getId();
+	}
+
+	public function setLu()
+	{
+		require_once BDD_CONFIG;
+		try{
+			DB::update(PREFIX_BDD.static::$BDDName, array("lu" => true), "id=%i", $this->id);
+			$this->values['lu'] = true;
+		} catch(MeekroDBException $e) {
+			EC::addBDDError($e->getMessage(), static::$BDDName."/setLu");
+			return false;
+		}
+		EC::add(static::$BDDName."/setLu : Succès.");
+		return true;
+	}
+
+	public function getDestName()
+	{
+		// utile pour construire la réponse lors d'une insertion de message
+		require_once BDD_CONFIG;
+		try {
+			$out = DB::queryFirstRow("SELECT CONCAT(nom,' ',prenom) as fullname FROM ".PREFIX_BDD."users WHERE id = %i",$this->values['idDest']);
+			if ($out !== null) {
+				return $out["fullname"];
+			} else {
+				return "?";
+			}
+		} catch(MeekroDBException $e) {
+			if (BDD_DEBUG_ON) return array('error'=>true, 'message'=>"#User/getList : ".$e->getMessage());
+			return "?";
+		}
 	}
 }
 
