9c70499876663b11f24d1a6cedca400962578ecf
gromax <gomaxx@gmail.com>
2018-10-10 18:00:42 +0200

    ajout des constantes permettant la connexion cas ajout du http:// pour le chemin au site

--- php/constantes.php
+++ php/constantes.php
@@ -16,13 +16,17 @@
 	// mails
 	define("PSEUDO_FROM","Exercices de maths");
 	define("EMAIL_FROM","exercices@goupill.fr");
-	define("PATH_TO_SITE","exercices.goupill.fr");
+	define("PATH_TO_SITE","http://exercices.goupill.fr");
 	define("NOM_SITE","Exercices de maths sur goupill.fr");
 	define("SMTP_PASSWORD","zwdHpjudYfv3CbEbt8Aa");
 	define("SMTP_USER","exercices@goupill.fr");
 	define("SMTP_HOST","mail.goupill.fr");
 	define("SMTP_PORT",465);
 
+	// cas
+	define("PATH_TO_AUTH", "http://exercices.goupill.fr/api/auth");
+	define("PATH_TO_ENT_CAS","https://ent.iledefrance.fr/cas/login?service=http://exercices.goupill.fr/api/auth");
+	define("PATH_TO_ENT_CAS_VALIDATE","https://ent.iledefrance.fr/cas/serviceValidate");
 
 	// Calcul de notes
 	//define("POIDS_GEO", 0.8);
