3d0090b14a525e8cb8606261309aa127f12a7c88
gromax <gomaxx@gmail.com>
2018-10-18 16:05:26 +0200

    ajout domain pour charger le mail en fonction de cas si mail pas précisé lors d'un chargement d'élèves par csv

--- php/constantes.php
+++ php/constantes.php
@@ -27,6 +27,7 @@
 	define("PATH_TO_AUTH", "http://exercices.goupill.fr/api/auth");
 	define("PATH_TO_ENT_CAS","https://ent.iledefrance.fr/cas/login?service=http://exercices.goupill.fr/api/auth");
 	define("PATH_TO_ENT_CAS_VALIDATE","https://ent.iledefrance.fr/cas/serviceValidate");
+	define("CAS_MAIL_DOMAIN","ent.iledefrance.fr");
 
 	// Calcul de notes
 	//define("POIDS_GEO", 0.8);
