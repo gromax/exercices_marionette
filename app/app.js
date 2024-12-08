4dc3a9d0d9cf6e9437e295c389f7dc43615771eb
gromax <gomaxx@gmail.com>
2024-12-08 19:13:06 +0100

    passage en 2.3

--- app/app.js
+++ app/app.js
@@ -53,7 +53,7 @@ define(["marionette","jquery-ui","bootstrap"], function(Marionette){
 
 
 	Manager.on("start", function(){
-		Manager.version = "2.2.398";
+		Manager.version = "2.3.0";
 		var historyStart = function() {
 			require([
 				"apps/header/header_app",
